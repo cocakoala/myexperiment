da43b892a6d12d2d85dafbcb4c7ea5e50ef52fc7
xj <xj@xj.cn>
2013-05-27 23:38:47 +0800

    增加bichunk提取的类

---
+++ MNP/src/xj/ExtractBiChunks.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package xj;
+
+/**
+ * @author xj
+ *
+ */
+public class ExtractBiChunks {
+
+	public String chunkingPath = "";
+	public String alignPath = "";
+	public String chnPath = "";
+	public String engChunkPath = "";
+	public String chnChunkPath = "";
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
