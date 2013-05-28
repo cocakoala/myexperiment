58e743ec375be1359423a64599e3148ae58049ed
xj <xj@xj.cn>
2013-05-29 00:30:41 +0800

    regular

--- MNP/src/xj/ExtractBiChunks.java
+++ MNP/src/xj/ExtractBiChunks.java
@@ -9,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -17,18 +18,18 @@ import java.util.List;
  */
 public class ExtractBiChunks {
 
-	public String chunkingPath = "";
-	public String alignPath = "";
-	public String chnPath = "";
-	public String engChunkPath = "";
-	public String chnChunkPath = "";
+	public String chunkingPath = "/home/xj/Documents/myexperiment/chunk-concerned/english.chunking.with-s.head1000";
+	public String alignPath = "/home/xj/Documents/myexperiment/chunk-concerned/Alignment.kickbracket.head1000";
+	public String chnPath = "/home/xj/Documents/myexperiment/chunk-concerned/chinese.kickbracket.head1000";
+	public String engChunkPath = "/home/xj/Documents/myexperiment/chunk-concerned/chnChunk";
+	public String chnChunkPath = "/home/xj/Documents/myexperiment/chunk-concerned/engChunk";
 	
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		new ExtractBiChunks().extract();
 	}
 	
 	public void extract() {
@@ -54,7 +55,7 @@ public class ExtractBiChunks {
 					chnIdx[i] = -1; alnIdx[i] = -1;
 				}
 				String[] alnAry = align.split(" ");
-				for ( int i = 0; i < align.length(); i++ ) {
+				for ( int i = 0; i < alnAry.length; i++ ) {
 					String[] tmp = alnAry[i].split("-");
 					int engPos = Integer.parseInt(tmp[1]);	// Pos for position
 					int chnPos = Integer.parseInt(tmp[0]);
@@ -65,9 +66,14 @@ public class ExtractBiChunks {
 				// OK, all the above is preparation. now we are going to extract chunks from this sentence pair.
 				String chunkInfo = null;
 				chnChkIdx.clear(); alnChkIdx.clear();
-				while ( ( chunkInfo = brAln.readLine() ) != null ) {
-					if ( chunkInfo.length() < 1 ) 
+				StringBuilder chnChunk = new StringBuilder();
+				StringBuilder engChunk = new StringBuilder();
+				while ( ( chunkInfo = brChk.readLine() ) != null ) {
+					
+					if ( chunkInfo.length() < 1 )
 						break;
+					if ( chunkInfo.startsWith("#") )
+						continue;
 					String[] chunkInfoAry = chunkInfo.split("[ ]+");
 					if ( chunkInfoAry[1].startsWith("O") || 
 						 chunkInfoAry[1].startsWith("C") ) {
@@ -75,6 +81,22 @@ public class ExtractBiChunks {
 					} else {
 						chnChkIdx.add( chnIdx[Integer.parseInt( chunkInfoAry[0] )] );
 						alnChkIdx.add( alnIdx[Integer.parseInt( chunkInfoAry[0] )] );
+						engChunk.append( chunkInfoAry[2] + " " );
+						if ( chunkInfoAry[1].startsWith("E") ) {	// end of a chunk
+							Collections.sort(chnChkIdx);
+							Collections.sort(alnChkIdx);
+							if ( isAlignConsecutive( alnChkIdx ) ) {
+								chnChunk = new StringBuilder();
+								for ( int i = chnChkIdx.get(0); i < chnChkIdx.get( chnChkIdx.size() - 1 ); i++ ) {
+									chnChunk.append( chnAry[i] + " " );
+								}
+								// TODO write chunk to file
+								System.out.println(chnChunk + " -- " + engChunk);
+							}
+							chnChunk.delete( 0, chnChunk.length() );
+							engChunk.delete( 0, engChunk.length() );
+							chnChkIdx.clear(); alnChkIdx.clear();
+						}
 					}
 				}
 			}
@@ -86,6 +108,15 @@ public class ExtractBiChunks {
 			
 		}
 		
+	}
+	
+	/**
+	 * judge if the numbers in the list are consecutive
+	 * @param list A sorted list in natural order
+	 * @return
+	 */
+	public boolean isAlignConsecutive( List<Integer> list ) {
+		return ( list.size() == ( list.get( list.size()-1 ) - list.get(0) ) );
 	}
 
 }
